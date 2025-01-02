c815538adf5e18b4b31c6cc6546a996b14c2c69b
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2025-01-02 22:07:36 +0100

    feat: add categories schema on store

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="categories-container">
+        <div className="category-container">
+          {/* Image */}
+          <div className="category-body-container">
+            <h2>Category A</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
 
-export default App
+        <div className="category-container">
+          {/* Image */}
+          <div className="category-body-container">
+            <h2>Category B</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/* Image */}
+          <div className="category-body-container">
+            <h2>Category C</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/* Image */}
+          <div className="category-body-container">
+            <h2>Category D</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/* Image */}
+          <div className="category-body-container">
+            <h2>Category E</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
