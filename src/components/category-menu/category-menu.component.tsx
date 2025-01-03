c097ff235cbc240c10295cb8c1b8f4d011a5ce4d
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2025-01-03 19:09:54 +0100

    chore: move data to app component

--- src/components/category-menu/category-menu.component.tsx
+++ src/components/category-menu/category-menu.component.tsx
@@ -1,35 +1,9 @@
 import React from 'react'
 import CategoryItem from '../category-item/category-item.component';
 
-const categories = [
-    {
-        id: 1,
-        title: "Category A",
-        imageUrl: "https://picsum.photos/500/300",
-    },
-    {
-        id: 2,
-        title: "Category B",
-        imageUrl: "https://picsum.photos/200/300",
-    },
-    {
-        id: 3,
-        title: "Category C",
-        imageUrl: "https://picsum.photos/200/300",
-    },
-    {
-        id: 4,
-        title: "Category D",
-        imageUrl: "https://picsum.photos/200/300",
-    },
-    {
-        id: 5,
-        title: "Category E",
-        imageUrl: "https://picsum.photos/200/300",
-    },
-];
 
-const CategoryMenu = () => {
+
+const CategoryMenu = ({ categories }) => {
     return (
         <div className="categories-container">
             {categories.map((category) => (
