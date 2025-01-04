9b713a170944b9404dff85bc6f12e09f6d401d7c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2025-01-04 11:41:18 +0100

    feat: add home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import React from 'react';
-import CategoryMenu from './components/category-menu/category-menu.component';
-
-const categories = [
-  {
-    id: 1,
-    title: "Category A",
-    imageUrl: "https://picsum.photos/500/300",
-  },
-  {
-    id: 2,
-    title: "Category B",
-    imageUrl: "https://picsum.photos/200/300",
-  },
-  {
-    id: 3,
-    title: "Category C",
-    imageUrl: "https://picsum.photos/200/300",
-  },
-  {
-    id: 4,
-    title: "Category D",
-    imageUrl: "https://picsum.photos/200/300",
-  },
-  {
-    id: 5,
-    title: "Category E",
-    imageUrl: "https://picsum.photos/200/300",
-  },
-];
+import Home from './routes/home/home.component';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <CategoryMenu categories={categories}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 };
---
+++ src/routes/home/home.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import CategoryMenu from './../../components/category-menu/category-menu.component'
+
+const categories = [
+    {
+        id: 1,
+        title: "Category A",
+        imageUrl: "https://picsum.photos/500/300",
+    },
+    {
+        id: 2,
+        title: "Category B",
+        imageUrl: "https://picsum.photos/200/300",
+    },
+    {
+        id: 3,
+        title: "Category C",
+        imageUrl: "https://picsum.photos/200/300",
+    },
+    {
+        id: 4,
+        title: "Category D",
+        imageUrl: "https://picsum.photos/200/300",
+    },
+    {
+        id: 5,
+        title: "Category E",
+        imageUrl: "https://picsum.photos/200/300",
+    },
+];
+
+const Home = () => {
+    return(
+        <>
+            <CategoryMenu categories={categories} />
+        </>
+    );
+}
+
+export default Home
