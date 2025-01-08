da5cc8c787dcd0827ef49281eb33fde9b774a3b8
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2025-01-08 12:13:43 +0100

    feat: add some style for sign in component

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -3,6 +3,7 @@ import {
   createUserDocumentFromAuth
 } from './../../utils/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import './sign-in.styles.scss'
 
 const SignIn = () => {
 
@@ -17,10 +18,16 @@ const SignIn = () => {
   }
 
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <SignUpForm />
+    <div className="signin-container">
+      <div className="signin-content">
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser} className="signin-button">
+          Sign in with Google Popup
+        </button>
+      </div>
+      <div className="signup-form-wrapper">
+        <SignUpForm />
+      </div>
     </div>
   )
 }
